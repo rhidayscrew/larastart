e6e7a486474105c459424c049eeb965b44ed082a
rhidayscrew17 <rhidayscrew@gmail.com>
2018-09-14 02:36:01 +0700

    Installing Laravel Passport for API Auth in Laravel 5.7 and add middleware usercontroler

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -88,7 +88,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 
           <li class="nav-item has-treeview ">
-            <a href="#" class="nav-link active">
+            <a href="" class="nav-link active">
               <i class="nav-icon fa fa-cog green"></i>
               <p>
                 Management
@@ -109,6 +109,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
+            <router-link to="/developer" class="nav-link">
+              <i class="nav-icon fas fa-cogs"></i>
+              <p>
+               Developer
+
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item">
             <router-link to="/profile" class="nav-link">
               <i class="nav-icon fas fa-user orange"></i>
               <p>
@@ -117,6 +127,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
+
                         <li class="nav-item">
                                 <a class="nav-link" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
